742b0a40867d1031c45573d787a1a19bef84b5cc
ram-rogers <ram23rogers@gmail.com>
2024-01-28 21:49:36 +0530

    login design

--- src/components/Login.js
+++ src/components/Login.js
@@ -8,6 +8,12 @@ const Login = () => {
                 <Content>
                     <CTA>
                         <CTALogoOne src="/images/cta-logo-one.svg" />
+                        <Signup>GET ALL THERE</Signup>
+                        <Description>
+                            Get Premier Access to Raya and the Last Draggon for an additional fee with a Disney+ subscription. As of 03/26/21, the price of Disney+ and The Disney Bundle will increase by $1.
+                        </Description>
+                        <CTALogoTwo src="/images/cta-logo-two.png" />
+
                     </CTA>
                     <BgImage>
 
@@ -60,6 +66,10 @@ const BgImage = styled.div`
 `
 
 const CTA = styled.div`
+    max-width: 650px;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
 
 
 `
@@ -69,6 +79,45 @@ const CTALogoOne = styled.img`
     max-width: 600px;
     min-height: 1px;
     display: block;
+    width: 100%;
+
+`
+
+const Signup = styled.a`
+    font-weight: bold;
+    color: #f9f9f9;
+    background-color: #0063e5;
+    margin-bottom: 12px;
+    width: 100%;
+    letter-spaciong: 1.5px;
+    font-size: 18px;
+    padding: 16.5px 0;
+    border: 1px solid transparent;
+    border-radius: 4px;
+    cursor: pointer;
+
+    &:hover{
+        background-color: #0483ee;
+    }
+
+
+`
+
+const Description = styled.p`
+    color: hsla(0,0%,95.3%,1);
+    font-size: 14px;
+    margin: 0 0 24px;
+    line-height: 1.5;
+    letter-spacing: 1.5px;
+
+
+`
+
+const CTALogoTwo = styled.img`
+    max-width: 600px;
+    margin-bottom: 20px;
+    display: inline-block;
+    vertical-align: bottom;
     width: 100%;
 
 `
