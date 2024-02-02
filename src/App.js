e466b9a17f57069faf2e8054f8dabf126ecf1c30
ram-rogers <ram23rogers@gmail.com>
2024-02-02 23:03:48 +0530

    header started

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './components/Login';
 import Home from './components/Home';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Header />
         <Routes>
-          <Route element={<Login />} path="/" />
+
+          <Route element={<><Login /></>} path="/" />
           <Route element={<Home />} path="/home" />
 
         </Routes>
